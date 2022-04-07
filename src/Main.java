d424fc8c190188bdb52b8c4a9806dc63dd9baf81
Fajar Putra <fajar.putra1691@gmail.com>
2022-04-07 08:36:51 +0700

    Kuis PBO

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pil;
+
+        System.out.println("+-------------------------------+");
+        System.out.println("| Pendaftaran Beasiswa Benang  |");
+        System.out.println("+-------------------------------+");
+        System.out.println("Terdapat 2 Program beasiswa yang di buka : ");
+        System.out.println("1. Beasiswa Pelajar");
+        System.out.println("2. Beasiswa Mahasiswa");
+        System.out.println("Pilih Beasiswa yang ingin di ambil : ");
+        pil = input.nextInt();
+
+        if(pil==1){
+
+        }
+    }
+}
